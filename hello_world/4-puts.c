77b59aeb7340323545e107609df045046f641303
root <agustinvm88@gmail.com>
2025-06-18 16:31:29 +0000

    Add 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,11 @@
 /**
  * main - Entry point
  *
- * Retur: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void){
-	puts("Programming is the link building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
