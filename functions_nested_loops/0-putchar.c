17a0476fa9b55a272725980c26f91e4ec723b6b3
root <agustinvm88@gmail.com>
2025-06-20 15:41:44 +0000

    Add 0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Imprime _putchar seguido de una nueva linea
+ *
+ * Return: 0 (exito)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
