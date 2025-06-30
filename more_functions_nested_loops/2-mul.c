b6bbf6ca441ec5b574e58253e7992f9d7bcba8b2
Agustin Vidal <agustinvm88@gmail.com>
2025-06-30 14:55:18 +0000

    Add 1-isdigit.c and 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  mul - multiplies two integers.
+ * @a: first int
+ * @b: second int
+ *
+ * Return: multiplication of int a and b.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
