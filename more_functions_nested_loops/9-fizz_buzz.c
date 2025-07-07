92a36506b0f42e64040a5657050e2d996b2e2c2d
Agustin Vidal <agustinvm88@gmail.com>
2025-07-07 01:54:02 +0000

    Add 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 	while (x <= 100)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (x % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (x % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d ", x);
 		x++;
