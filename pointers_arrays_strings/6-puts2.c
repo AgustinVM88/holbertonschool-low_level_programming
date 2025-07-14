02af7446bd706ebc3197de45eb606899c2fe41a4
Agustin Vidal <agustinvm88@gmail.com>
2025-07-14 00:05:27 +0000

    Add 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
  */
 void puts2(char *str)
 {
-	int x = 0;
+	int x;
 
-	while (str[x] != '\0')
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		_putchar(str[x]);
-		x += 2;
+		if (x % 2 == 0)
+			_putchar(str[x]);
 	}
 	_putchar('\n');
 }
