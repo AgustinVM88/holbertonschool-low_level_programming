df7edff1dbc0073a3a2ee3660f89082deccb7dcd
Agustin Vidal <agustinvm88@gmail.com>
2025-07-14 22:57:56 +0000

    Add 3-strcmp.c

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
-	char *str;
-	
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	
+	char s1[] = "Hello";
+	char s2[] = "World!";
+
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
 	return (0);
 }
