105d1fa5c5b19ebbc160387c40a497952d9b78cd
Agustin Vidal <agustinvm88@gmail.com>
2025-07-21 01:50:26 +0000

    Add 4-pow_recursion.c

--- recursion/4-main.c
+++ recursion/4-main.c
@@ -8,11 +8,19 @@
  */
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
-	
-	t = _strpbrk(s, f);
-	printf("%s\n", t);
+	int r;
+
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	r = _pow_recursion(1024, 0);
+	printf("%d\n", r);
+	r = _pow_recursion(2, 16);
+	printf("%d\n", r);
+	r = _pow_recursion(5, 2);
+	printf("%d\n", r);
+	r = _pow_recursion(5, -2);
+	printf("%d\n", r);
+	r = _pow_recursion(-5, 3);
+	printf("%d\n", r);
 	return (0);
 }
