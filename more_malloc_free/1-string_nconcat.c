e281cda9b8b176c40ad74ad374137c54b3186bf0
Agustin Vidal <agustinvm88@gmail.com>
2025-07-22 07:43:29 +0000

    Add 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,13 +8,13 @@
  * @s2: second string
  * @n: maximum number of bytes to concatenate from s2
  *
- * Return: pointer to the new string, or NULL if it fails
+ * Return: pointer to the new string, or NULL if it fails malloc
  *
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_str;
-	unsigned int x, y, len1 = 0, len2 = 0;
+	unsigned int x, y, len1 = 0, len2 = 0, total_len;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -25,17 +25,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-	/* Si n es mayor que la longitud de s2, usar toda s2 */
+	/* Si n es mayor o igual a len2, usar todo s2 */
 	if (n >= len2)
 		n = len2;
-	/* Reservar memorias para la nueva cadena + null terminator */
-	new_str = malloc(sizeof(char) * (len1 + n + 1));
+	/* total de memoria a reservar = len1 + n + 1 para '\0' */
+	total_len = len1 + n;
+
+	new_str = malloc(sizeof(char) * (total_len + 1));
 	if (new_str == NULL)
 		return (NULL);
 	/* Copiar s1 a new_str */
-	for (x = 0; x <= len1; x++)
+	for (x = 0; x < len1; x++)
 		new_str[x] = s1[x];
-	/* Copia los primeros n caracteres de s2 */
+	/* Copia n caracteres de s2 */
 	for (y = 0; y < n; y++)
 		new_str[x + y] = s2[y];
 	/* Agrega NULL al terminar */
