377671558fe4000966edb39cde1e9187b8f09cfa
Agustin Vidal <agustinvm88@gmail.com>
2025-07-22 09:32:37 +0000

    Add dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - new name for struct dog
+ */
+typedef struct dog dog_t;
+
 /* Prototipo de la duncion init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
