5e4ce338f2f7ce44f075d4ff33c3cdd44c6cf025
Agustin Vidal <agustinvm88@gmail.com>
2025-07-22 06:15:17 +0000

    Add 4-free_grid.c

--- malloc_free/4-main.c
+++ malloc_free/4-main.c
@@ -1,26 +1,53 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - check the code 
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
+{
+	    int w;
+	        int h;
+
+		    h = 0;
+		        while (h < height)
+				    {
+					            w = 0;
+						            while (w < width)
+								            {
+										                printf("%d ", grid[h][w]);
+												            w++;
+													            }
+							            printf("\n");
+								            h++;
+									        }   
+}
+
+/**
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
+	    int **grid;
 
-	r = _pow_recursion(1, 10);
-	printf("%d\n", r);
-	r = _pow_recursion(1024, 0);
-	printf("%d\n", r);
-	r = _pow_recursion(2, 16);
-	printf("%d\n", r);
-	r = _pow_recursion(5, 2);
-	printf("%d\n", r);
-	r = _pow_recursion(5, -2);
-	printf("%d\n", r);
-	r = _pow_recursion(-5, 3);
-	printf("%d\n", r);
-	return (0);
+	        grid = alloc_grid(6, 4);
+		    if (grid == NULL)
+			        {
+					        return (1);
+						    }
+		        print_grid(grid, 6, 4);
+			    printf("\n");
+			        grid[0][3] = 98;
+				    grid[3][4] = 402;
+				        print_grid(grid, 6, 4);
+					    free_grid(grid, 4);
+					        return (0);
 }
