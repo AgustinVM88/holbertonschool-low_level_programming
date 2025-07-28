80d790bb7c7262109a4b056f3c0eff5794ed274a
Agustin Vidal <agustinvm88@gmail.com>
2025-07-28 00:21:43 +0000

    Add 2-append_text_to_file.c

--- file_io/2-main.c
+++ file_io/2-main.c
@@ -1,46 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int ac, char **av)
 {
-    list_t *head;
+	    int res;
 
-    head = NULL;
-    add_node(&head, "Alexandro");
-    add_node(&head, "Asaia");
-    add_node(&head, "Augustin");
-    add_node(&head, "Bennett");
-    add_node(&head, "Bilal");
-    add_node(&head, "Chandler");
-    add_node(&head, "Damian");
-    add_node(&head, "Daniel");
-    add_node(&head, "Dora");
-    add_node(&head, "Electra");
-    add_node(&head, "Gloria");
-    add_node(&head, "Joe");
-    add_node(&head, "John");
-    add_node(&head, "John");
-    add_node(&head, "Josquin");
-    add_node(&head, "Kris");
-    add_node(&head, "Marine");
-    add_node(&head, "Mason");
-    add_node(&head, "Praylin");
-    add_node(&head, "Rick");
-    add_node(&head, "Rick");
-    add_node(&head, "Rona");
-    add_node(&head, "Siphan");
-    add_node(&head, "Sravanthi");
-    add_node(&head, "Steven");
-    add_node(&head, "Tasneem");
-    add_node(&head, "William");
-    add_node(&head, "Zee");
-    print_list(head);
-    return (0);
+	        if (ac != 3)
+			    {
+				            dprintf(2, "Usage: %s filename text\n", av[0]);
+					            exit(1);
+						        }
+		    res = append_text_to_file(av[1], av[2]);
+		        printf("-> %i)\n", res);
+			    return (0);
 }
